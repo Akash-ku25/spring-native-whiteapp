b7be391d0377737e5ef3fccfbc1aa1c4b1451f2f
Akash-ku25 <42737394+Akash-ku25@users.noreply.github.com>
2022-05-26 15:53:14 +0530

    created whiteapp

---
+++ src/main/java/com/example/whiteapp/WhiteappApplication.java
@@ -0,0 +1,13 @@
+package com.example.whiteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whiteapp/WhiteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whiteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
